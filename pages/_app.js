54da4b76dc41970a00d3b1a59f58dee967d6ce7a
The_Viper <swagatk1004@gmail.com>
2024-07-31 16:24:20 +0530

    reusable components setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css';
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+import '../styles/globals.css';
+import { Footer, Navbar } from '../components';
+
+const App = ({ Component, pageProps }) => (
+
+  <ThemeProvider attribute="class" defaultTheme="light">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/a59328838e.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
 export default App;
